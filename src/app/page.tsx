3840676e18a52996dec69e8f02729a285ac4b0e2
André Oliveira <132096135+AFCOliveira@users.noreply.github.com>
2025-01-02 11:14:39 -0300

    feat: stucture html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <main>
+    <main>
+      <div className="header">
         <Image
           src="/next.svg"
           alt="Next.js logo"
@@ -11,7 +11,36 @@ export default function Home() {
           height={38}
           priority
         />
-      </main>
-    </div>
+        <div>
+          <h1>Hi, i´m André</h1>
+          <h2>Software Developer</h2>
+        </div>
+        <div className="experience">
+          <h3>Experience</h3>
+          <p>texto</p>
+        </div>
+        <div className="experience-time">
+
+        </div>
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="languages-info">
+            <span>🇧🇷 PT-BR - Native Speaker</span>
+            <span>🇺🇸 EN - Intermediary</span>
+            <span>🇪🇸 ES - Intermediary</span>
+          </div>
+          <h3>Education</h3>
+          <div className="educational-info"></div>
+          <span>🎓</span>
+          <span>Information Systems Bachelors - Universidade FUMEC</span>
+        </div>
+        <div className="buttons">
+          <div className="social-media">
+
+          </div>
+          <button>Contact Me</button>
+        </div>
+      </div>
+    </main>
   );
 }

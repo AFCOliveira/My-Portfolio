1f7729356c86d5ced00f1b6dc5dffd80d9c8eece
André Oliveira <132096135+AFCOliveira@users.noreply.github.com>
2025-01-02 17:10:10 -0300

    fix: adjustments

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -4,7 +4,10 @@ export function Experience() {
   return (
     <div className="experience">
       <SectionTitle text="Experience" />
-      <p>Software developer, with working experience in Front-end, Back-end development</p>
+      <p>Software developer, with working experience in Front-end and Back-end,
+        focusing on full stack software development, I have been working on personal
+        projects that have allowed me to apply and deepen my knowledge in specific technologies,
+        such as React, Node.js.</p>
     </div>
   )
 }
